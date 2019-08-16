38627977c294a86539466015a7ab52c32ff68783
Admin <mrthanksgiving@163.com>
2019-08-16 18:35:04 +0800

    第三方插件暂时屏蔽

--- lib/playground/navigation/action_bar.dart
+++ lib/playground/navigation/action_bar.dart
@@ -1,6 +1,5 @@
 import 'package:firewood/playground/util/fcolor.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
 class ActionBarWidget extends StatelessWidget {
   final String title;
